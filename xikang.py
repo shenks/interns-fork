18c38983f25393ee53f17e6d15b2b8d570a8aa04
Shenks Lee <shenkslee@gmail.com>
2025-01-28 16:18:54 +0800

    add py files

---
+++ xikang.py
@@ -0,0 +1,7 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    # change the statement within double quotes here
+    print(f"Welcome {name}, intern of Winsys year 2023")
