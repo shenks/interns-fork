4268a27c383f35b63f0d274534d6135dfcac0a22
rxly05 <rylie0131@gmail.com>
2025-02-04 10:43:04 +0800

    update test from Jaden

--- jaden.py
+++ jaden.py
@@ -4,4 +4,4 @@ def greet(name):
 if __name__ == "__main__":
     name = input("Enter your name: ")
     # change the statement within double quotes here
-    print(f"Welcome {name}, intern of Winsys year 2023")
+    print(f"Welcome {name}, intern of Winsys year 2024")
